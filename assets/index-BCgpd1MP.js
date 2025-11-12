3a74a7acd65be8bbfe47b61a866092b077614c84
ClaudiaZewil <49198720+ClaudiaZewil@users.noreply.github.com>
2025-11-12 13:54:36 +0100

    Updates

--- assets/index-BCgpd1MP.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DFbzVms4.js","assets/index-BpZ6DzXH.js","assets/index-DSXSLoHg.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-BpZ6DzXH.js";const _=e("Browser",{web:()=>o(()=>import("./web-DFbzVms4.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};

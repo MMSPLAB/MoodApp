59538b641825568b2cbb5f00c99bf7444c2c17a5
AngioG <72987164+AngioG@users.noreply.github.com>
2026-02-25 11:33:48 +0100

    Revert "Feb 26"

--- src/components/SetUp/02IntroduzioneEsperimento.jsx
+++ src/components/SetUp/02IntroduzioneEsperimento.jsx
@@ -1,54 +1,28 @@
 import React, { useState } from "react";
 import { Button } from "@mui/material";
-import { useLocation, useNavigate } from 'react-router'
+import { useNavigate } from 'react-router'
 import WestSharpIcon from '@mui/icons-material/WestSharp';
 import EastSharpIcon from '@mui/icons-material/EastSharp';
-import CloseIcon from '@mui/icons-material/Close';
 
 function IntroduzioneEsperimento() {
     const navigate = useNavigate();
-    const location = useLocation();
-
-    //se si arriva dalla home, allora passiamo uno state
-    const fromHome = location.state?.fromHome === true;
-
     return (
         <div>
-            {!fromHome ? (
-                <>
-                    <div className="arrow-left">
-                        <Button variant="outlined" onClick={() => navigate("/user-ID")}>
-                            <WestSharpIcon />
-                        </Button>
-                    </div>
-                </>
-            ) : (
-                <div className="close-icon">
-                    <Button variant="text" onClick={() => navigate("/")}>
-                        <CloseIcon />
-                    </Button>
-                </div>
-            )}
-
-            <div className="contenitore-testo">
-                <h1>Introduzione</h1>
-                <p>
-                    Ti diamo il benvenuto nell'applicazione per il monitoraggio dell'<span className="blue-text">Umore (mood)</span>!<br /><br />
-                    Questa applicazione è uno strumento fondamentale per supportare la ricerca scientifica sul monitoraggio e l’analisi degli stati affettivi.
-                    Attraverso il tuo contributo, aiuterai a raccogliere dati essenziali per comprendere meglio le dinamiche emotive e le interazioni tra mood, stimoli esterni e segnali fisiologici.<br /><br />
-                    L’esperimento si articola in diverse fasi, a partire dalla tua profilazione iniziale, che include domande su caratteristiche personali e risposte a specifici questionari.<br /><br />
-                    Successivamente, ti verranno proposti stimoli visivi ed esercizi giornalieri per monitorare il tuo stato d’animo in tempo reale, raccogliendo dati utili alla ricerca.<br /><br />
-                    Grazie per aver aderito!
-                </p>
+            <div className="arrow-left">
+                <Button variant="outlined" onClick={() => navigate("/user-ID")}> {/* ← */}<WestSharpIcon/> </Button>
             </div>
-
-            {!fromHome && (
-                <div className="arrow-right">
-                    <Button variant="contained" onClick={() => navigate("/informativa-privacy")}>
-                        <EastSharpIcon />
-                    </Button>
-                </div>
-            )}
+            <h1>Introduzione</h1>
+            <p>
+                Ti diamo il benvenuto nell'applicazione per il monitoraggio dell'<span className="blue-text">Umore (mood)</span>!<br /><br />
+                Questa applicazione è uno strumento fondamentale per supportare la ricerca scientifica sul monitoraggio e l’analisi degli stati affettivi.
+                Attraverso il tuo contributo, aiuterai a raccogliere dati essenziali per comprendere meglio le dinamiche emotive e le interazioni tra mood, stimoli esterni e segnali fisiologici.<br /><br />
+                L’esperimento si articola in diverse fasi, a partire dalla tua profilazione iniziale, che include domande su caratteristiche personali e risposte a specifici questionari.<br /><br />
+                Successivamente, ti verranno proposti stimoli visivi ed esercizi giornalieri per monitorare il tuo stato d’animo in tempo reale, raccogliendo dati utili alla ricerca.<br /><br />
+                Grazie per aver aderito!
+            </p>
+            <div className="arrow-right">
+                <Button variant="contained" onClick={() => navigate("/informativa-privacy")}>{/* → */}<EastSharpIcon/></Button>
+            </div>
         </div>
     )
 }
--- src/components/SetUp/07Generalità.jsx
+++ src/components/SetUp/07Generalità.jsx
@@ -3,7 +3,6 @@ import { Button, FormControl, FormControlLabel, TextField, Radio, RadioGroup } f
 import { useNavigate } from 'react-router'
 import WestSharpIcon from '@mui/icons-material/WestSharp';
 import EastSharpIcon from '@mui/icons-material/EastSharp';
-import safeStorage from "../../../safeStorage";
 
 function Generality() {
 
@@ -17,11 +16,9 @@ function Generality() {
     const [dispositivo, setDispositivo] = useState("");
     const [savedDispositivo, setSavedDispositivo] = useState("");
 
-    const [error, setError] = useState("");
-
 
     useEffect(() => {
-        const storedAge = safeStorage.getItem("Età");
+        const storedAge = localStorage.getItem("Età");
         if (storedAge) {
             setAge(storedAge);
             setSavedAge(storedAge);
@@ -29,7 +26,7 @@ function Generality() {
     }, []);
 
     useEffect(() => {
-        const storedGender = safeStorage.getItem("Genere");
+        const storedGender = localStorage.getItem("Genere");
         if (storedGender) {
             setGender(storedGender);
             setSavedGender(storedGender);
@@ -37,7 +34,7 @@ function Generality() {
     }, []);
 
     useEffect(() => {
-        const storedIstruzione = safeStorage.getItem("Istruzione");
+        const storedIstruzione = localStorage.getItem("Istruzione");
         if (storedIstruzione) {
             setIstruzione(storedIstruzione);
             setSavedIstruzione(storedIstruzione);
@@ -45,7 +42,7 @@ function Generality() {
     }, []);
 
     useEffect(() => {
-        const storedDispositivo = safeStorage.getItem("Dispositivo");
+        const storedDispositivo = localStorage.getItem("Dispositivo");
         if (storedDispositivo) {
             setDispositivo(storedDispositivo);
             setSavedDispositivo(storedDispositivo);
@@ -55,129 +52,103 @@ function Generality() {
     const handleGenderChange = (event) => {
         const selectedGender = event.target.value;
         setGender(selectedGender);
+        console.log(selectedGender);
     };
 
     const handleAgeChange = (event) => {
-        const input = event.currentTarget.value;
-        setAge(input);
-
-        if (input === "") {
-            setError("");
-            return;
-        }
-
-        const ageNumber = parseInt(input, 10);
-        if (isNaN(ageNumber)) {
-            setError("L'età deve essere un numero valido");
-        } else if (ageNumber < 18) {
-            setError("Devi essere amggiorenne per partecipare all'esperimento");
-        } else if (ageNumber > 115) {
-            setError("L'età inserita sembra essere troppo alta");
-        } else {
-            setError("");
-        }
+        const newAge = event.currentTarget.value;
+        setAge(newAge);
+        console.log(newAge);
     };
-
 
     const handleIstruzioneChange = (event) => {
         const newIstruzione = event.currentTarget.value;
         setIstruzione(newIstruzione);
+        console.log(newIstruzione);
     }
 
     const handleAltroChange = (event) => {
         const newAltro = event.currentTarget.value;
         setAltro(newAltro);
+        console.log(newAltro);
         const newIstruzione = event.currentTarget.value;
         setIstruzione(newIstruzione);
+        console.log(newIstruzione);
     }
 
     const handleDispositivoChange = (event) => {
         const newDispositivo = event.currentTarget.value;
         setDispositivo(newDispositivo);
+        console.log(newDispositivo);
     }
 
     const saveAll = (event) => {
-        //event.preventDefault(); //esigenze di Safari: non usiamo componente form
-        safeStorage.setItem("Genere", gender);
+        event.preventDefault();
+        localStorage.setItem("Genere", gender);
         setSavedGender(gender);
-
-        safeStorage.setItem("Età", age);
+        console.log("Genere confermato e salvato:", gender);
+        localStorage.setItem("Età", age);
         setSavedAge(age);
-
-        safeStorage.setItem("Istruzione", istruzione);
+        console.log("Età confermata e salvata:", age);
+        localStorage.setItem("Istruzione", istruzione);
         setIstruzione(istruzione);
-
-        safeStorage.setItem("Dispositivo", dispositivo);
+        console.log("Istruzione confermata e salvata:", istruzione);
+        localStorage.setItem("Dispositivo", dispositivo);
         setDispositivo(dispositivo);
+        console.log("Dispositivo confermato e salvato:", dispositivo);
     };
 
     const navigate = useNavigate();
     return (
         <div>
             <div className="arrow-left">
-                <Button variant="outlined" onClick={() => navigate("/termini-e-condizioni")}>   <WestSharpIcon /> </Button>
+                <Button variant="outlined" onClick={() => navigate("/selezione-orario-notifiche")}> {/* ← */}<WestSharpIcon /> </Button>
             </div>
-            <div className="contenitore-testo">
-                <div className="titolo-generalità">
-                    <h1>Generalità</h1>
-                </div>
-                <div className="form-generalità">
-                    <form onSubmit={saveAll}>
-                        <div className="gender">
-                            <FormControl>
-                                <h3>Genere</h3>
-                                <RadioGroup aria-labelledby="demo-error-radios" name="gender" value={gender} onChange={handleGenderChange}>
-                                    <FormControlLabel value="maschio" control={<Radio />} label="Maschio" />
-                                    <FormControlLabel value="femmina" control={<Radio />} label="Femmina" />
-                                    <FormControlLabel value="non-binario" control={<Radio />} label="Non binario" />
-                                    <FormControlLabel value="preferisco-non-specificarlo" control={<Radio />} label="Preferisco non specificarlo" />
-                                </RadioGroup>
-                            </FormControl>
-                        </div>
-                        <br />
-                        <div className="age">
-                            <h3>Età</h3>
-                            <TextField required variant="outlined" placeholder="Age" value={age} name="Age" onChange={handleAgeChange} /> <br />
-                            {error && (
-                                <div style={{ color: "red", marginTop: "4px" }}>
-                                    {error}
-                                </div>
-                            )}
-                        </div>
-                        <br />
-                        <div className="istruzione">
-                            <FormControl>
-                                <h3>Istruzione</h3>
-                                <RadioGroup aria-labelledby="demo-error-radios" name="istruzione" value={istruzione} onChange={handleIstruzioneChange}>
-                                    <FormControlLabel value="Diploma scuola secondaria di primo grado (medie)" control={<Radio />} label="Diploma scuola secondaria di primo grado (medie)" />
-                                    <FormControlLabel value="Diploma scuola secondaria di secondo grado (superiori)" control={<Radio />} label="Diploma scuola secondaria di secondo grado (superiori)" />
-                                    <FormControlLabel value="Laurea triennale" control={<Radio />} label="Laurea triennale" />
-                                    <FormControlLabel value="Laurea magistrale" control={<Radio />} label="Laurea magistrale" />
-                                    <FormControlLabel value="Laurea a ciclo unico" control={<Radio />} label="Laurea a ciclo unico" />
-                                    <FormControlLabel value="Master" control={<Radio />} label="Master" />
-                                    <FormControlLabel value="Dottorato" control={<Radio />} label="Dottorato" />
-                                    {/* L'opzione non risulta selezionata fino a quando non viene scritto qualcosa all'interno del campo */}
-                                    <FormControlLabel control={<Radio checked={istruzione === altro && altro !== ""} onChange={() => setIstruzione(altro)} />} label={<input className="input-istruzione" type="text" label="Altro..." value={altro ?? ""} placeholder="Altro..." onChange={handleAltroChange} />} />
-                                </RadioGroup>
-                            </FormControl>
-                        </div>
-                        <br />
-                        <div className="modello-telefono">
-                            <h3>Dispositivo</h3>
-                            <p>Quale dispositivo stai utilizzando?</p>
-                            <p><i>Specifica da quale dispositivo stai compilando questo questionario (ad esempio, scrivi la marca del tuo telefono).</i></p>
-
-                            <TextField required variant="outlined" placeholder="Marca dispositivo" value={dispositivo} name="Dispositivo" onChange={handleDispositivoChange} /> <br />
-                        </div>
-                        <br />
-                        <div className="red">
-                            <p>*completa tutti i campi prima di procedere.</p>
-                        </div>
-                        <div className="arrow-right">
-                            <Button variant="contained" disabled={!gender || age === "" || isNaN(age) || age < 18 || age > 115 || !istruzione || !dispositivo} type="submit" onClick={() => navigate("/scelta-avatar")}> <EastSharpIcon /></Button>
-                        </div>
-                    </form>
-                </div>
+            <div className="titolo-generalità">
+                <h1>Generalità</h1>
+            </div>
+            <div className="form-generalità">
+                <form onSubmit={saveAll}>
+                    <div className="gender">
+                        <FormControl>
+                            <h3>Genere</h3>
+                            <RadioGroup aria-labelledby="demo-error-radios" name="gender" value={gender} onChange={handleGenderChange}>
+                                <FormControlLabel value="maschio" control={<Radio />} label="Maschio" />
+                                <FormControlLabel value="femmina" control={<Radio />} label="Femmina" />
+                                <FormControlLabel value="non-binario" control={<Radio />} label="Non binario" />
+                                <FormControlLabel value="preferisco-non-specificarlo" control={<Radio />} label="Preferisco non specificarlo" />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                    <div className="age">
+                        <h3>Età</h3>
+                        <TextField required variant="outlined" placeholder="Age" value={age} name="Age" onChange={handleAgeChange} /> <br />
+                    </div>
+                    <div className="istruzione">
+                        <FormControl>
+                            <h3>Istruzione</h3>
+                            <RadioGroup aria-labelledby="demo-error-radios" name="istruzione" value={istruzione} onChange={handleIstruzioneChange}>
+                                <FormControlLabel value="Diploma scuola secondaria di secondo grado (medie)" control={<Radio />} label="Diploma scuola secondaria di secondo grado (medie)" />
+                                <FormControlLabel value="Diploma scuola secondaria di primo grado (liceo)" control={<Radio />} label="Diploma scuola secondaria di primo grado (liceo)" />
+                                <FormControlLabel value="Laurea triennale" control={<Radio />} label="Laurea triennale" />
+                                <FormControlLabel value="Laurea magistrale" control={<Radio />} label="Laurea magistrale" />
+                                <FormControlLabel value="Laurea a ciclo unico" control={<Radio />} label="Laurea a ciclo unico" />
+                                <FormControlLabel value="Master" control={<Radio />} label="Master" />
+                                <FormControlLabel value="Dottorato" control={<Radio />} label="Dottorato" />
+                                {/* L'opzione non risulta selezionata fino a quando non viene scritto qualcosa all'interno del campo */}
+                                <FormControlLabel control={<Radio checked={istruzione === altro && altro !== ""} onChange={() => setIstruzione(altro)} />} label={<input className="input-istruzione" type="text" label="Altro..." value={altro ?? ""} placeholder="Altro..." onChange={handleAltroChange}/>} />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                    <div className="modello-telefono">
+                        <h3>Dispositivo</h3>
+                        <p>Quale dispositivo stai utilizzando? Scrivi la marca.</p>
+                        <TextField required variant="outlined" placeholder="Marca dispositivo" value={dispositivo} name="Dispositivo" onChange={handleDispositivoChange} /> <br />
+                    </div>
+                    <div className="arrow-right">
+                        <Button variant="contained" disabled={!gender || !age || !istruzione || !dispositivo} type="submit" onClick={() => navigate("/scelta-avatar")}>{/* → */}<EastSharpIcon /></Button>
+                    </div>
+                </form>
             </div>
         </div >
     )
